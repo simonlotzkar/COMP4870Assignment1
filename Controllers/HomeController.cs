6039741fdae169ea8be1494c56a4feaba9291a71
Kohei Dunnet <kdunnet1@my.bcit.ca>
2025-02-03 18:03:40 -0800

    Names are at the bottom of the Home Page

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -15,6 +15,7 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
+        ViewData["FooterText"] = "Kohei Dunnet, Simon Lotzkar and Ben Nguyen";
         return View();
     }
 
